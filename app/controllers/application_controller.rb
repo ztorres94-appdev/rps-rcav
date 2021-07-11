5c61bf0ee4c2a83bc68c0cb06f94ac35218e281a
ztorres94 <ztorres@chicagobooth.edu>
2021-07-11 22:29:52 +0000

    first test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,11 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+  def play_rock
+    self.redirect_to("https://www.wikipedia.org")
+  end
 
+
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-
+get("/rock", { :controller => "application", :action => "play_rock" })
 
 end
